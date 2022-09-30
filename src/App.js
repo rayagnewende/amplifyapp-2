ad1e7a3e9c680cb11f3da94c7ed70ba391cfc22e
kabore r evariste <kaboreva@gmail.com>
2022-09-30 16:19:23 +0200

    ajout du système d'authentification à l'appli grace amplify

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
 
 function App() {
-  return (
+  return (   
     <div className="App">
-      <header className="App-header">
+      <header>
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Bienvenue dans notre react firts app with AWS Amplify</h1>
+        <h1>We now have Auth!</h1>
       </header>
+      <AmplifySignOut />
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
